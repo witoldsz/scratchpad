60c133c840a896f7854e30b6df4c27862cb9d9cd
Witold Szczerba <witold.szczerba@currency-one.com>
2018-09-18 11:35:34 +0200

    4

---
+++ README.md
@@ -0,0 +1,2 @@
+# How to launch it
+    $ mvn package -DskipTests && java -jar target/superhero-0.0.1-SNAPSHOT-fat.jar
--- src/main/java/pl/superhero/MainVerticle.java
+++ src/main/java/pl/superhero/MainVerticle.java
@@ -10,6 +10,7 @@ import static io.vertx.core.Future.future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.api.contract.RouterFactoryOptions;
 import java.util.List;
 import static java.util.Optional.ofNullable;
 
@@ -22,54 +23,47 @@ public class MainVerticle extends AbstractVerticle {
   public void start(Future done) {
     System.setProperty("org.mongodb.async.type", "netty");
     mongo = MongoClient.createShared(vertx, new JsonObject()
-        .put("connection_string",
-            "mongodb://witoldsz:Q0PV6aCrggdTz1Kv"
-            + "@superhero-shard-00-00-ty8ms.mongodb.net:27017,"
-            + "superhero-shard-00-01-ty8ms.mongodb.net:27017,"
-            + "superhero-shard-00-02-ty8ms.mongodb.net:27017"
-            + "/testing"
-            + "?ssl=true"
-            + "&replicaSet=superhero-shard-0"
-            + "&authSource=admin"
-            + "&retryWrites=true"
-        )
+      .put("connection_string",
+        "mongodb://witoldsz:Q0PV6aCrggdTz1Kv"
+        + "@superhero-shard-00-00-ty8ms.mongodb.net:27017,"
+        + "superhero-shard-00-01-ty8ms.mongodb.net:27017,"
+        + "superhero-shard-00-02-ty8ms.mongodb.net:27017"
+        + "/testing"
+        + "?ssl=true"
+        + "&replicaSet=superhero-shard-0"
+        + "&authSource=admin"
+        + "&retryWrites=true"
+      )
     );
 
+    // Just to make sure the connection works
     Future<List<String>> mongoF = future();
     mongo.getCollections(mongoF.completer());
 
     mongoF
-        .compose(collections -> {
-          System.out.println("Collections: " + collections);
-          Future<OpenAPI3RouterFactory> routerFactoryF = future();
-          OpenAPI3RouterFactory.createRouterFactoryFromFile(
-              this.vertx,
-              getClass().getResource("/superheroes.json").getFile(),
-              r -> {
-                System.out.println("r!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 = " + r);
-              }
-          );
-          return routerFactoryF;
-        })
-        .compose(routerFactory -> {
+      .compose(collections -> {
+        Future<OpenAPI3RouterFactory> routerFactoryF = future();
+        OpenAPI3RouterFactory.create(this.vertx, "superheroes.json", routerFactoryF.completer());
+        return routerFactoryF;
+      })
+      .compose(routerFactory -> {
+        System.out.println("routerFactory: " + routerFactory);
 
-        }, done);
-//        .compose(routerFactory -> {
-//          System.out.println("routerFactory: " + routerFactory);
-//          // Enable automatic response when ValidationException is thrown
-//          routerFactory.enableValidationFailureHandler(true);
-//
-//          // Add routes handlers
-//          routerFactory.addHandlerByOperationId("listHeroes", new pl.superhero.handlers.ListHeroesHandler());
-//          routerFactory.addHandlerByOperationId("createHero", new pl.superhero.handlers.CreateHeroHandler());
-//          routerFactory.addHandlerByOperationId("showHeroById", new pl.superhero.handlers.ShowHeroByIdHandler());
-//
-//          // Generate the router
-//          Router router = routerFactory.getRouter();
-//          server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost"));
-//          server.requestHandler(router::accept).listen();
-//          done.complete();
-//        }, done);
+        routerFactory.setOptions(new RouterFactoryOptions()
+          .setMountNotImplementedHandler(true)
+          .setMountValidationFailureHandler(true));
+
+        // Add routes handlers
+        routerFactory.addHandlerByOperationId("listHeroes", new pl.superhero.handlers.ListHeroesHandler());
+        routerFactory.addHandlerByOperationId("createHero", new pl.superhero.handlers.CreateHeroHandler());
+        routerFactory.addHandlerByOperationId("showHeroById", new pl.superhero.handlers.ShowHeroByIdHandler());
+
+        // Generate the router
+        Router router = routerFactory.getRouter();
+        server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost"));
+        server.requestHandler(router::accept).listen();
+        done.complete();
+      }, done);
   }
 
   @Override
