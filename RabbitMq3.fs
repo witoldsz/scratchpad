bef0efef077c9a0977e9cfdf9474816f189d725c
Witold Szczerba <witold.szczerba@currency-one.com>
2020-04-05 19:23:20 +0200

    Multithreading issues

--- RabbitMq3.fs
+++ RabbitMq3.fs
@@ -8,7 +8,7 @@ open System.Threading
 open MQ3
 open Utils
 
-module RabbitMQ3 =
+module internal RabbitMQ3 =
 
     type private QueueProps =
         { name: string
@@ -19,7 +19,7 @@ module RabbitMQ3 =
           autoDelete: bool
           autoAck: bool }
 
-        static member Query(name): QueueProps =
+        static member Query name: QueueProps =
             { name = name
               exchangeName = "amq.topic"
               prefetchCount = 10
@@ -28,7 +28,7 @@ module RabbitMQ3 =
               autoDelete = true
               autoAck = true }
 
-        static member Event(name, prefetchCount): QueueProps =
+        static member Event name prefetchCount: QueueProps =
             { name = name
               exchangeName = "amq.topic"
               prefetchCount = prefetchCount
@@ -37,16 +37,13 @@ module RabbitMQ3 =
               autoDelete = false
               autoAck = false }
 
-    type private RabbitMQueue (ch: IModel, p: QueueProps) =
+    type private RabbitMQueue (conn: IConnection, p: QueueProps) =
 
-        do ch.BasicQos(prefetchSize = 0u, prefetchCount = uint16 p.prefetchCount, ``global`` = true)
-        do ch.QueueDeclare(queue = p.name, durable = p.durable, exclusive = p.exclusive, autoDelete = p.autoDelete)
-            |> ignore
+        let receivingChannel = conn.CreateModel()
 
-        member val bindings: Binding list = List.empty with get, set
-        member val consumer = EventingBasicConsumer(ch)
+        let mutable bindings: Binding list = List.empty
 
-        member this.onEvent(event: BasicDeliverEventArgs) =
+        let onEvent (event: BasicDeliverEventArgs) =
             let _, traceHeader = event.BasicProperties.Headers.TryGetValue("trace")
             let tracePoints =
                 match traceHeader with
@@ -64,39 +61,46 @@ module RabbitMQ3 =
 
             let body = Encoding.UTF8.GetString(event.Body)
             Async.Start (async {
-                do! this.bindings
+                do! bindings
                     |> List.tryFind (fun b -> b.Matches routingKey)
                     |> Option.map (fun b -> b.Consumer body trace)
                     |> Option.defaultValue (async { return () })
 
                 if p.autoAck
-                then ch.BasicAck(deliveryTag = event.DeliveryTag, multiple = false)
+                then receivingChannel.BasicAck(deliveryTag = event.DeliveryTag, multiple = false)
             })
+
+        do
+            receivingChannel.BasicQos(prefetchSize = 0u, prefetchCount = uint16 p.prefetchCount, ``global`` = true)
+            receivingChannel.QueueDeclare
+                (queue = p.name, durable = p.durable, exclusive = p.exclusive, autoDelete = p.autoDelete)
+            |> ignore
 
         interface IMQueue with
 
             member this.Bind(bindingKey, consumer) =
                 logInfo "MQ: binding queue %s to %s" p.name bindingKey
-                ch.QueueBind(p.name, p.exchangeName, bindingKey)
-                this.bindings <- Binding(bindingKey, consumer) :: this.bindings
+                receivingChannel.QueueBind(p.name, p.exchangeName, bindingKey)
+                bindings <- Binding(bindingKey, consumer) :: bindings
                 this :> IMQueue
 
             member this.Done() =
+                let eventConsumer = EventingBasicConsumer(receivingChannel)
+                eventConsumer.Received.Add onEvent
+                receivingChannel.BasicConsume(queue = p.name, autoAck = p.autoAck, consumer = eventConsumer) |> ignore
                 logInfo "MQ: binding queue %s done" p.name
-                this.consumer.Received.Add(this.onEvent)
-                ch.BasicConsume(queue = p.name, autoAck = p.autoAck, consumer = this.consumer) |> ignore
 
-    type private RabbitMq3 (moduleName: string, conn: IConnection, publishChannel: IModel) as this =
+    type private RabbitMq3 (moduleName: string, cf: ConnectionFactory) as this =
 
-        let mq3 = this :> IMQ3
+        let conn = cf.CreateConnection()
+        let publishChannel = conn.CreateModel()
+        let queries = Collections.Concurrent.ConcurrentDictionary<Guid, Body -> unit>()
 
-        member val queries: Map<Guid, Body -> unit> = Map.empty with get, set
-
-        member this.Queue(p: QueueProps) =
+        let queue (p: QueueProps) =
             logInfo "MQ: creating queue %s" p.name
-            RabbitMQueue(conn.CreateModel(), p)
+            RabbitMQueue(conn, p) :> IMQueue
 
-        member this.CreatePublishProps(trace: Trace) =
+        let createPublishProps (trace: Trace) =
             let props = publishChannel.CreateBasicProperties()
             props.Headers <- Map.ofList [
                 "trace", trace.TracePoints :> obj
@@ -106,64 +110,65 @@ module RabbitMQ3 =
             props.ContentType <- "application/json"
             props
 
+        let publish exchange routingKey props (body: string) =
+            fun () -> publishChannel.BasicPublish(exchange, routingKey, props, Encoding.UTF8.GetBytes body)
+            |> lock publishChannel
+
+        do
+            (this :> IMQ3).QueryQueue(moduleName)
+                .Bind("ping", fun body trace -> failwith "TODO…")
+                .Bind(moduleName, fun body trace -> async {
+                    return queries
+                    |> DictUtil.tryRemove(trace.CorrelationId)
+                    |> Option.map (fun resolve -> resolve body)
+                    |> Option.defaultValue ()
+                })
+                .Done()
+
         interface IMQ3 with
 
             member this.QueryQueue name =
-                this.Queue(QueueProps.Query(name)) :> IMQueue
+                queue (QueueProps.Query name)
 
             member this.EventQueue name prefetchCount =
-                this.Queue(QueueProps.Event(name, prefetchCount)) :> IMQueue
+                queue (QueueProps.Event name prefetchCount)
 
             member this.Publish routingKey body oldTrace =
                 let trace = oldTrace.Next()
-                let props = this.CreatePublishProps(trace)
+                let props = createPublishProps trace
                 props.Persistent <- true
-                publishChannel.BasicPublish("amq.topic", routingKey, props, Encoding.UTF8.GetBytes body)
+                publish "amq.topic" routingKey props body
                 trace
 
             member this.PublishResponse oldTrace body =
                 let trace = oldTrace.Next()
-                let props = this.CreatePublishProps(trace)
+                let props = createPublishProps trace
                 props.Persistent <- false
                 props.CorrelationId <- trace.CorrelationId.ToString()
-                publishChannel.BasicPublish("amq.topic", trace.ReplyTo, props, Encoding.UTF8.GetBytes body)
+                publish "" trace.ReplyTo props body
 
             member this.Query routingKey body oldTrace =
                 let trace = oldTrace.Next()
                 let correlationId = Guid.NewGuid()
-                let props = this.CreatePublishProps(trace)
+                let props = createPublishProps trace
                 props.ReplyTo <- moduleName
                 props.CorrelationId <- correlationId.ToString()
                 props.Persistent <- false
-                publishChannel.BasicPublish("amq.topic", routingKey, props, Encoding.UTF8.GetBytes body)
+                publish "amq.topic" routingKey props body
                 Async.FromContinuations (fun (resolve, reject, _) ->
                     let timeout = async {
                         do! Async.Sleep 3000
-                        this.queries <- this.queries.Remove(correlationId)
-                        reject (System.TimeoutException (sprintf "routing key: %s query body: %O" routingKey body)) // TODO
+                        queries.TryRemove correlationId |> ignore
+                        reject (TimeoutException (sprintf "routing key: %s query body: %O" routingKey body)) // TODO
                     }
                     let cts = new CancellationTokenSource()
                     let resolveAndCancelTimeout result =
                         cts.Cancel()
-                        this.queries <- this.queries.Remove(correlationId)
                         resolve result
 
                     Async.Start (timeout, cts.Token)
-                    this.queries <- this.queries.Add(correlationId, resolveAndCancelTimeout)
+                    queries.TryAdd(correlationId, resolveAndCancelTimeout) |> ignore
                 )
 
-    let connect (moduleName:string) (cf: ConnectionFactory) =
-        let conn = cf.CreateConnection()
-        let publishChannel = conn.CreateModel()
-        let rmq3 = RabbitMq3(moduleName, conn, publishChannel)
-        let mq3 = rmq3 :> IMQ3
-        mq3.QueryQueue(moduleName)
-            .Bind("ping", fun body trace -> failwith "TODO…")
-            .Bind(moduleName, fun body trace -> async {
-                return trace.CorrelationId
-                |> rmq3.queries.TryFind
-                |> Option.map (fun resolve -> resolve body)
-                |> Option.defaultValue ()
-            })
-            .Done()
-        mq3
+    let connect (moduleName:string) (cf: RabbitMQ.Client.ConnectionFactory) =
+        RabbitMq3 (moduleName, cf) :> IMQ3
--- Utils.fs
+++ Utils.fs
@@ -1,5 +1,14 @@
 namespace CurrencyOne.Infra
 
-module Utils =
+module internal Utils =
     let logInfo =
         printfn
+
+    module DictUtil =
+
+        open System.Collections
+
+        let tryRemove key (dict: Concurrent.ConcurrentDictionary<'a, 'b>) =
+            match dict.TryRemove(key) with
+            | (true, v) -> Some v
+            | (false, _) -> None
